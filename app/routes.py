e83832e2a471524b9fb54bdc45437aa0b65a99c7
ron h <hjron@protonmail.com>
2026-01-07 10:41:40 -0600

    finish chapter two

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'hi ron'
+    user = {'username': 'ronnyaldo'}
+    posts = [
+        {
+            'author': {'username': 'john'},
+            'body': 'nice day in mpls'
+        },
+        {
+            'author': {'username': 'susan'},
+            'body': 'can\'t wait for new season of Pitt!!'
+        }
+    ]
+    return render_template('index.html', title='home', user=user, posts=posts)
