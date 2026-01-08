30c0c64496d6538ab75d762ad61cc86580a73f81
ron h <hjron@protonmail.com>
2026-01-07 19:33:57 -0600

    finish Chapter five

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember me')
     submit = SubmitField('sign in')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'repeat password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('register')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(
+            User.username == username.data))
+        if user is not None:
+            raise ValidationError('please use a different username.')
+
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(
+            User.email == email.data))
+        if user is not None:
+            raise ValidationError('please use a different email address.')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
+from app.models import User
+from urllib.parse import urlsplit
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'ronnyaldo'}
     posts = [
         {
             'author': {'username': 'john'},
@@ -16,13 +20,40 @@ def index():
             'body': 'can\'t wait for new season of Pitt!!'
         }
     ]
-    return render_template('index.html', title='home', user=user, posts=posts)
+    return render_template('index.html', title='home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = db.session.scalar(
+                sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('invalid username or password')
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='sign in', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('congrats, you\'re registered!!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='register', form=form)
